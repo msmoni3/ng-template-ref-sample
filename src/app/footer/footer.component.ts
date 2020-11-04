37f7ac520f568d6de017be0e22381f036aee70c8
msmoni3 <msmoni3@gmail.com>
2020-11-04 20:32:16 +0530

    3 files modified

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,27 +1,23 @@
-import { Component, OnInit,ElementRef,ViewChild , Input } from '@angular/core';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, ElementRef, ViewChild, Input } from "@angular/core";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
   faHeart = faHeart;
-  @ViewChild("tv") tv : ElementRef;
-  @Input("temp") temp : number;
+  @ViewChild("tv") tv: ElementRef;
+  @Input("temp") temp: number;
 
-  constructor() { 
-    
+  constructor() {}
+
+  get getPathName(): string {
+    return window.location.pathname;
   }
 
-  get fontawesomeInUrl(): boolean {
-    return !window.location.hostname.includes('fontawesome');
-  }
   ngOnInit() {
-    // console.log(this.editInUrl);
-    console.log(window.location.hostname.includes('fontawesome'));
+   
   }
-
-
 }
